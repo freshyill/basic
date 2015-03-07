b18dfcb6bfc63311e6d0627332b17766f4597d67
Chris Coleman <ccoleman@aaas.org>
2015-03-07 13:07:52 -0500

    Added build modes

--- Readme.markdown
+++ Readme.markdown
@@ -10,10 +10,27 @@ Just a basic project starter for the things I tend to do a lot of
 * A pretty low-key Bower setup, with only Normalize and jQuery
 * A fairly un-opinionated EditorConfig setup
 
+## Installation
+
+    npm install
+
+    bower install
+
+## Running
+
+## Production Mode
+
+    gulp
+
+This will output compressed styles
+
+## Development Mode
+
+    gulp --dev
+
+This will output nested styles
 
 ## Todo
 
-* Set up some Javascript starters
 * Put a little more effort into the EditorConfig
-* Make sure I'm not forgetting anything with LibSass
 * Turn this into a Jekyll thing, maybe?
--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,91 @@
-var gulp        = require('gulp'),
-    $           = require('gulp-load-plugins')(),
-    browserSync = require('browser-sync'),
-    reload      = browserSync.reload;
+var gulp         = require('gulp'),
+    $            = require('gulp-load-plugins')(),
+    browserSync  = require('browser-sync'),
+    pngquant     = require('imagemin-pngquant'),
+    reload       = browserSync.reload,
+    production   = true,
+    prefixes     = ['last 2 version', 'safari 5', 'ie 8', 'opera 12.1', 'ios 6', 'android 4', 'Firefox >= 4'],
+    cssStyle     = "compressed",
+    bsProxy      = "demo.dev";
 
-var paths = {
-  scss: ['./scss/*.scss'],
-   css: './css'
-};
+if ($.util.env.dev === true) {
+  production = false,
+  cssStyle   = "nested";
+}
 
 var changeEvent = function(evt) {
   $.util.log('File', $.util.colors.cyan(evt.path.replace(new RegExp('/.*(?=/' + '/source' + ')/'), '')), 'was', $.util.colors.magenta(evt.type));
 };
 
-// Sass
+var paths = {
+  images: {
+      src: 'img/**/*.{jpg,jpeg,png,gif,svg}',
+     dest: 'build/img/'
+  },
+  scripts: {
+      src: 'js/**/*.js',
+     skip: '',
+     dest: 'build/js/'
+  },
+  styles: {
+      src: 'scss/**/*.scss',
+     dest: 'build/css'
+  }
+};
+
+
+// Styles
 gulp.task('styles', function() {
-  gulp.src(paths.scss)
+  gulp.src(paths.styles.src)
     .pipe($.sourcemaps.init())
     .pipe($.size())
-    .pipe($.using())
     .pipe($.sass({
-      outputStyle: "compressed",
+      outputStyle: cssStyle,
       onError: function(error) {
         $.util.beep();
         $.util.log("\n\n[" + $.util.colors.green("gulp-sass") +"]\n" + $.util.colors.cyan(error.message) + " on " + $.util.colors.yellow("line " + error.line) + " in\n" + error.file);
       }
     }))
-    .pipe($.autoprefixer())
-    .pipe($.sourcemaps.write('./maps'))
-    .pipe(gulp.dest(paths.css))
+    .pipe($.autoprefixer(prefixes))
+    .pipe($.sourcemaps.write("."))
+    .pipe(gulp.dest(paths.styles.dest))
     .pipe($.filter('*.css'))
-    .pipe(reload({
-      stream: true
-    }));
+    .pipe(reload({ stream: true }));
 });
 
 
-// BrowserSync
-gulp.task("browsersync", function() {
+// Images
+gulp.task("images", function() {
+  gulp.src(paths.images.src)
+    .pipe($.using())
+    .pipe($.size())
+    .pipe($.imagemin({
+      progressive: true,
+      svgoPlugins: [{removeViewBox: false}],
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest(paths.images.dest));
+});
+
+
+// Package Scripts
+gulp.task('scripts', function () {
+  return gulp.src(paths.scripts.src)
+    .pipe($.changed(paths.scripts.dest))
+    .pipe($.using())
+    .pipe(production ? $.uglify() : $.util.noop())
+    .pipe($.size({title: 'scripts'}))
+    .pipe(gulp.dest(paths.scripts.dest))
+    .pipe($.filter('*.js'))
+    .pipe($.notify('Scripts task complete'))
+    .pipe(reload({ stream: true }));
+});
+
+
+// browserSync
+gulp.task("browserSync", function() {
   browserSync({
-    proxy: "demo.dev",
+    proxy: bsProxy,
     ui: {
       port: 8080
     },
@@ -46,9 +93,22 @@ gulp.task("browsersync", function() {
   });
 });
 
+
+
+// browserSync Reload
+gulp.task('reload', function () {
+  reload();
+});
+
 // Watch
-gulp.task('watch', ['styles', 'browsersync'], function() {
-  gulp.watch(paths.scss, ['styles']).on('change', function(evt) {
+gulp.task('watch', ['styles', 'scripts', 'images', 'browserSync'], function() {
+  gulp.watch(paths.styles.src, ['styles']).on('change', function(evt) {
+    changeEvent(evt);
+  });
+  gulp.watch(paths.scripts.src, ['scripts']).on('change', function(evt) {
+    changeEvent(evt);
+  });
+  gulp.watch(paths.images.src, ['images']).on('change', function(evt) {
     changeEvent(evt);
   });
 });
